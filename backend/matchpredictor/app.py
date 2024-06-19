28e41e7c02f344eb0ad76a7b27c2d16fa651de9b
lmarte17 <lmarte17@gmail.com>
2024-06-19 19:18:28 -0400

    Added a RandomForestPredictor class

--- backend/matchpredictor/app.py
+++ backend/matchpredictor/app.py
@@ -14,6 +14,7 @@ from matchpredictor.predictors.home_predictor import HomePredictor
 from matchpredictor.predictors.linear_regression_predictor import train_regression_predictor
 from matchpredictor.predictors.alphabet_predictor import AlphabetPredictor
 from matchpredictor.predictors.past_results_predictor import train_results_predictor
+from matchpredictor.predictors.random_forest_predictor import train_random_forest_predictor
 from matchpredictor.predictors.simulation_predictor import train_offense_and_defense_predictor, train_offense_predictor
 from matchpredictor.teams.teams_api import teams_api
 from matchpredictor.teams.teams_provider import TeamsProvider
@@ -31,7 +32,8 @@ def build_model_provider(training_data: List[Result]) -> ModelProvider:
         Model("Full simulator", train_offense_and_defense_predictor(training_data, 10_000)),
         Model("Alphabet Simulator", AlphabetPredictor()),
         # The linear regression model uses scikit learn, so can cause issues on some machines
-        Model("Linear regression", train_regression_predictor(training_data))
+        Model("Linear regression", train_regression_predictor(training_data)),
+        Model("Random Forest", train_random_forest_predictor(training_data))
     ])
 
 
